f035d49eb7966254b079768e3e02f3249aeda457
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-08-20 06:03:52 +0700

    🛠️ Pretty the course name

--- src/utility/parseGithubRepoToCourses.ts
+++ src/utility/parseGithubRepoToCourses.ts
@@ -1,9 +1,17 @@
 import { GithubRepo } from "@/types/GithubRepo";
 import { Courses } from "@/types/Course";
+import upperCaseFirstChar from "./upperCaseFirstChar";
 
 const BASE_URL = "https://extra-web-design-smk-muh-1-klaten-utara.github.io";
 
 type ReturnType = Map<string, Courses>;
+
+// TODO : Should make some sort of dictionary to alias a string to something else
+function prettyCourseName(name: string, sep: string): string {
+    return name.split(sep)
+        .map((value) => upperCaseFirstChar(value))
+        .join(' ');
+}
 
 export default function (repoList: Array<GithubRepo>): ReturnType {
     let coursesList: ReturnType = new Map();
@@ -20,11 +28,17 @@ export default function (repoList: Array<GithubRepo>): ReturnType {
             return;
         }
 
+        // Pop the year and semester from name
+        const nameArray = element.name.split('-');
+        nameArray.pop();
+        nameArray.pop();
+        let name = nameArray.join(' ');
+
         let key = regex[0];
         if (coursesList.has(key)) {
             coursesList.get(key)?.listCourses.push({
                 id: element.id.toString(),
-                name: element.name,
+                name: prettyCourseName(name, ' '),
                 deploy_link: `${BASE_URL}/${element.name}`,
             });
 
@@ -34,12 +48,12 @@ export default function (repoList: Array<GithubRepo>): ReturnType {
         let split = key.split("-");
         coursesList.set(key, {
             year: parseInt(split[0]),
-            semester: split[1],
+            semester: upperCaseFirstChar(split[1]),
             totalCourses: 1,
             listCourses: [
                 {
                     id: element.id.toString(),
-                    name: element.name,
+                    name: prettyCourseName(name, ' '),
                     deploy_link: `${BASE_URL}/${element.name}`,
                 },
             ],
